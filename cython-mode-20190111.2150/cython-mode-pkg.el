f8ac47434a435495650e1b990f330e8c16cf1e8f
Ding Huiqiang <hqding@outlook.com>
2021-11-12 12:59:33 +0800

    feat(): Daily update

--- cython-mode-20190111.2150/cython-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from cython-mode.el  -*- no-byte-compile: t -*-
-(define-package "cython-mode" "20190111.2150" "Major mode for editing Cython files" 'nil :commit "3f56484704742c1af33c9737642125c55b0634d5")
---
+++ cython-mode-20211111.1407/cython-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from cython-mode.el  -*- no-byte-compile: t -*-
+(define-package "cython-mode" "20211111.1407" "Major mode for editing Cython files" 'nil :commit "b75af38ce5c309cd84c1835220932e53e9a9adb6")
