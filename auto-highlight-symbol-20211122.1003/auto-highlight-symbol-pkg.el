d58edc7531a10535d707fb329047c94ba16b476e
Ding Huiqiang <hqding@outlook.com>
2021-11-24 16:41:57 +0800

    feat(): Daily update

---
+++ auto-highlight-symbol-20211122.1003/auto-highlight-symbol-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from auto-highlight-symbol.el  -*- no-byte-compile: t -*-
+(define-package "auto-highlight-symbol" "20211122.1003" "Automatic highlighting current symbol minor mode" '((emacs "26.1") (ht "2.3")) :commit "59223a1377981a5c83011fbf323f07d169cf2db4" :authors '(("Mitsuo Saito" . "arch320@NOSPAM.gmail.com")) :maintainer '("Shen, Jen-Chieh" . "jcs090218@gmail.com") :keywords '("highlight" "face" "match" "convenience") :url "http://github.com/jcs-elpa/auto-highlight-symbol")
---
+++ org-ref-20211119.134/org-ref-pkg.el
@@ -0,0 +1,20 @@
+(define-package "org-ref" "20211119.134" "citations, cross-references and bibliographies in org-mode"
+  '((dash "0")
+    (s "0")
+    (f "0")
+    (htmlize "0")
+    (hydra "0")
+    (avy "0")
+    (parsebib "0")
+    (bibtex-completion "0")
+    (citeproc "0"))
+  :commit "d013e202c2b8b72a8d8b853f1fe4ec31d93e7174" :authors
+  '(("John Kitchin" . "jkitchin@andrew.cmu.edu"))
+  :maintainer
+  '("John Kitchin" . "jkitchin@andrew.cmu.edu")
+  :keywords
+  '("org-mode" "cite" "ref" "label")
+  :url "https://github.com/jkitchin/org-ref")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
