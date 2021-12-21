9f6d8c76183ba5f29790bd20ba46480d2e7852c1
Ding Huiqiang <hqding@outlook.com>
2021-12-21 10:31:50 +0800

    feat(): Daily update

---
+++ live-py-mode-20211216.329/live-py-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "live-py-mode" "20211216.329" "Live Coding in Python"
+  '((emacs "24.3"))
+  :commit "2b95d8bf61a64c4231f7d68a808e5c6ad8698c46" :authors
+  '(("Don Kirkby http://donkirkby.github.io"))
+  :maintainer
+  '("Don Kirkby http://donkirkby.github.io")
+  :keywords
+  '("live" "coding")
+  :url "http://donkirkby.github.io/live-py-plugin/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ pippel-20211205.1711/pippel-pkg.el
@@ -0,0 +1,12 @@
+(define-package "pippel" "20211205.1711" "Frontend to python package manager pip"
+  '((emacs "25.1")
+    (s "1.11.0")
+    (dash "2.12.0"))
+  :commit "5493309f17e7d30254e3832162f73b486079d12d" :authors
+  '(("Fritz Stelzer" . "brotzeitmacher@gmail.com"))
+  :maintainer
+  '("Arif Er" . "arifer612@protonmail.me")
+  :url "https://github.com/arifer612/pippel")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
